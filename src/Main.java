6cd8579b17b99a7e2f88a671906f78986fdc10bb
evii999 <gkvantov@gmail.com>
2023-05-09 12:26:02 +0500

    2ih4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     int a = 9;
-    
+    int b = 9;
+    int c = 9;
+    String result = "Done";
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
